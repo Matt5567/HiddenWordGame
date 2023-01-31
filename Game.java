e91c07ce750a60830b4c7a87d2ce47046d95b74d
mshanahan <mshanahan@student-bmhs-la.org>
2023-01-31 10:17:04 -0800

    final product

--- Game.java
+++ Game.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Game {
     private String hidden;
     private String guess;
-    private String hint;
+    private String hint = "";
+    private Scanner kb = new Scanner(System.in);
 
     public static void main(String[] args){
         Game game = new Game();
@@ -9,7 +12,39 @@ public class Game {
     }
 
     public void run(){
-        System.out.println("Hello");
+        System.out.println("Enter a hidden word");
+        hidden = kb.nextLine();
+
+            while(!hint.equals(hidden)) {
+                System.out.println("Enter a " + hidden.length() + " letter word to guess");
+                guess = kb.nextLine();
+                hint = "";
+                    for(int i = 0; i < hidden.length(); i++)
+                    {
+                        if(guess.substring(i, i+1).equals(hidden.substring(i, i+1))){
+                            hint += guess.substring(i, i+1);
+                        }
+
+                        else if(hidden.indexOf(guess.substring(i, i+1)) != -1){
+                            hint += "+";
+
+                    }
+                        else if(hidden.indexOf(guess.substring(i, i+1)) == -1 ){
+                            hint += "*";
+                        }
+
+
+
+                    }
+                System.out.println(hint);
+            }
+
+        System.out.println("Congratulations! You guessed the correct word.");
     }
 
 }
+
+
+
+
+
